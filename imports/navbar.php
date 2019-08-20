e17ff1b6d342408108450a1ebb50910322e04de0
Vic P <vic4key@gmail.com>
2019-08-21 01:46:33 +0700

    Initial

---
+++ imports/navbar.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-inverse navbar-fixed-top" style="margin-bottom: 0px !important">
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+				<span class="sr-only">Toggle Navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="<?php echo $GLOBALS["base"]; ?>">
+				<i class="fa fa-gg fa-lg" aria-hidden="true"></i>
+			</a>
+		</div>
+		<div id="navbar" class="collapse navbar-collapse">
+			<ul class="nav navbar-nav">
+				<li class="active">
+					<a href="#">
+						<i class="fa fa-home fa-lg" aria-hidden="true"></i>
+					</a>
+				</li>
+				<li>
+					<a id="mn_about" href="" data-toggle="modal" data-target="#md_modal">
+						<i class="fa fa-info-circle fa-lg" aria-hidden="true"></i>
+					</a>
+				</li>
+				<li>
+					<a target="_blank" href="<?php echo "https://github.com/".$GLOBALS["user"]."/".$GLOBALS["domain"]; ?>">
+						<i class="fa fa-star-o fa-lg" aria-hidden="true"></i>
+					</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
--- index.php
+++ index.php
@@ -46,25 +46,7 @@
 
 	<div class="loader"></div>
 
-	<nav class="navbar navbar-inverse navbar-fixed-top" style="margin-bottom: 0px !important">
-		<div class="container">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="<?php echo $GLOBALS["base"]; ?>">VICLAB</a>
-			</div>
-			<div id="navbar" class="collapse navbar-collapse">
-				<ul class="nav navbar-nav">
-					<li class="active"><a href="#">HOME</a></li>
-					<li><a id="mn_about" href="" data-toggle="modal" data-target="#md_modal">ABOUT</a></li>
-				</ul>
-			</div>
-		</div>
-	</nav>
+	<?php require_once("imports/navbar.php"); ?>
 
 	<div id="header" class="container">
 		<div class="row"></div>
