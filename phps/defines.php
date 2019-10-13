47d9ec27bd515a542c0808a404c5009ee8399417
Vic P <vic4key@gmail.com>
2019-10-13 20:43:43 +0700

    Initial

--- phps/defines.php
+++ phps/defines.php
@@ -4,18 +4,18 @@ $IS_LOCAL = $_SERVER["SERVER_NAME"] == "localhost" ? true : false;
 $BASE_URL = sprintf("%s://%s/", $_SERVER["SERVER_PORT"] == "80" ? "http" : "https", $_SERVER["SERVER_NAME"]);
 
 $GLOBALS = array_merge($GLOBALS, array(
-  "title"   => "Vic Lab",
+  "title"   => "Vic Online",
   "author"  => "Vic P.",
   "user"    => "vic4key",
   "email"   => "vic4key@gmail.com",
-  "domain"  => "viclab.biz",
+  "domain"  => "vic.onl",
   "token"   => "",
   "auth"    => "",
   "year"    => (date("Y")),
   "base"    => ($BASE_URL),
   "local"   => ($IS_LOCAL),
   "icon"    => ($IS_LOCAL ? "" : $BASE_URL)."images/favicon.ico",
-  "cover"   => ($IS_LOCAL ? "" : $BASE_URL)."images/ogimage.jpg",
+  "cover"   => ($IS_LOCAL ? "" : $BASE_URL)."images/cover.jpg",
   "avatar"  => ($IS_LOCAL ? "" : $BASE_URL)."images/avatar.jpg",
 ));
 
